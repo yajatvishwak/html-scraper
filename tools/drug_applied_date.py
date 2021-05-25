4bd373461e86574f9d27985d5a3ca6de8450a256
yajatvishwak <yajat472001@gmail.com>
2021-05-25 15:46:33 +0530

    👌 IMPROVE: added applied date

---
+++ tools/drug_applied_date.py
@@ -0,0 +1,27 @@
+import os
+from bs4 import BeautifulSoup
+if os.path.exists("./testfile_drug_applied_date.html"):
+    os.remove("./testfile_drug_applied_date.html")
+if os.path.exists("../processed/drug_applied_date.txt"):
+    os.remove("../processed/drug_applied_date.txt")
+
+count = 0
+string_arr = []
+
+
+with open("../htmlFile/index.html", "r") as html:
+    file = open("testfile_drug_applied_date.html", "a")
+    for line in html:
+        if "Applied date" in line:
+            count += 1
+            file.write('<div class="applieddate">' + line + "</div>\n")
+
+
+with open("testfile_drug_applied_date.html", "r") as html:
+    soup = BeautifulSoup(html, 'html.parser')
+    file = open("../processed/drug_applied_date.txt", "a")
+    for div in soup.find_all("div",
+                             attrs={
+                                 "class": "applieddate"}
+                             ):
+        file.write(div.text)
